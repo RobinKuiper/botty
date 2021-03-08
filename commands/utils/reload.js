0c3a86d8f0fb575d0eb38754455d5b008316058b
Robin Kuiper <robingjkuiper@gmail.com>
2021-03-08 11:43:34 +0100

    Basic Eventsystem

--- commands/utils/reload.js
+++ commands/utils/reload.js
@@ -8,21 +8,11 @@ module.exports = {
         const commandFolders = fs.readdirSync('./commands');
         
         if(!args.length){
-            for(const folder of commandFolders){
-                const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
-                for(const file of commandFiles){
-                    delete require.cache[require.resolve(`../${folder}/${file}`)];
-
-                    try{
-                        const command = require(`../${folder}/${file}`);
-                        message.client.commands.set(command.name, command);
-                        message.channel.send(`Command \`${command.name}\` was reloaded!`);
-                    }catch(error){
-                        console.error(error);
-                        message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
-                    }
-                }
-            }
+            reloadCommands(message, commandFolders);
+        }else if(args[0] === "events"){
+            reloadEvents(message);
+        }else if(args[0] === "commands"){
+            reloadCommands(message, commandFolders);
         }else{
             const commandName = args[0].toLowerCase();
             const command = message.client.commands.get(commandName)
@@ -46,3 +36,37 @@ module.exports = {
 
 	},
 };
+
+function reloadCommands(message, commandFolders){
+    for(const folder of commandFolders){
+        const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+        for(const file of commandFiles){
+            delete require.cache[require.resolve(`../${folder}/${file}`)];
+
+            try{
+                const command = require(`../${folder}/${file}`);
+                message.client.commands.set(command.name, command);
+                message.channel.send(`Command \`${command.name}\` was reloaded!`);
+            }catch(error){
+                console.error(error);
+                message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
+            }
+        }
+    }
+}
+
+function reloadEvents(message){
+        const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+        for(const file of eventFiles){
+            delete require.cache[require.resolve(`../../events/${file}`)];
+
+            try{
+                const event = require(`../../events/${file}`);
+                message.client.commands.set(event.name, event);
+                message.channel.send(`Event \`${event.name}\` was reloaded!`);
+            }catch(error){
+                console.error(error);
+                message.channel.send(`There was an error while reloading an event \`${event.name}\`:\n\`${error.message}\``);
+            }
+        }
+}
--- main.js
+++ main.js
@@ -4,11 +4,18 @@ const { token, prefix } = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.once('ready', () => {
-    console.log("Ready!");
-});
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
-const cooldowns = new Discord.Collection();
+for(const file of eventFiles){
+    const event = require(`./events/${file}`);
+    if(event.once){
+        client.once(event.name, (...args) => event.execute(...args, client));
+    }else{
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
+}
+
+client.cooldowns = new Discord.Collection();
 
 client.commands = new Discord.Collection();
 const commandFolders = fs.readdirSync('./commands');
@@ -20,66 +27,5 @@ for(const folder of commandFolders){
         client.commands.set(command.name, command);
     }
 }
-
-client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
-
-    const command = client.commands.get(commandName)
-        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-    if(!command) return;
-
-    if(command.permissions){
-        console.log("Command has permissions");
-        const authorPerms = message.channel.permissionsFor(message.author);
-        console.log(authorPerms);
-        if(!authorPerms || !authorPerms.has(command.permissions)){
-            return message.reply("You can not do this!");
-        }
-    }
-
-    if(command.guildOnly && message.channel.type === 'dm'){
-        return message.reply("I can't execute that command inside DMs!");
-    }
-
-    if(!cooldowns.has(command.name)){
-        cooldowns.set(command.name, new Discord.Collection());
-    }
-
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 3) * 1000;
-
-    if(timestamps.has(message.author.id)){
-        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-        if(now < expirationTime){
-            const timeLeft = (expirationTime - now) / 1000;
-            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-        }
-    }
-
-    timestamps.set(message.author.id, now);
-    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-    if(command.args && !args.length){
-        let reply = `You didn't provide any arguments.`;
-        if(command.usage){
-            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-        }
-
-        return message.channel.send(reply);
-    }
-
-    try{
-        command.execute(message, args);
-    } catch(err){
-        console.error(err);
-        message.reply('There was an error trying to execute that command!');
-    }
-});
 
 client.login(token);
