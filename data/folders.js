240651fa481da9f0cf09c253be46611d11db0250
Robin Kuiper <robingjkuiper@gmail.com>
2021-03-18 13:21:05 +0100

    Added basic data

---
+++ data/folders.js
@@ -0,0 +1,8 @@
+const path = require("path");
+
+module.exports = {
+  modules: path.join(__dirname, "../modules"),
+  events: path.join(__dirname, "../events"),
+  commands: path.join(__dirname, "../commands"),
+  config: path.join(__dirname, "../config"),
+};
